74c9abea5235e49f26c7798e64138e08812c688b
Suhas Julapalli <suhasjulapalli@gmail.com>
2022-07-28 12:18:22 -0400

    Design for dreamx front page nearly complete.

--- DreamXWebsite/App.js
+++ DreamXWebsite/App.js
@@ -1,16 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View} from 'react-native';
+import { Button, StyleSheet, Text, View, Image, ScrollView, useWindowDimensions } from 'react-native';
 import { useState } from 'react';
-
+import { TouchableWithoutFeedback } from 'react-native-web';
 
 const MyApp = () => {
+  const window = useWindowDimensions();
   return (
-      <View id="Parent View">
-        <View style={styles.dark}>
-          <p id="par"></p>
+    <ScrollView id="Parent View" style={{maxHeight: 2729}}>
+      <View style={[styles.dark, {justifyContent: 'center', height: 506, flexDirection: 'row'}]}>
+        <Text id="Page Header" style={{fontSize: 40, color: 'white', alignSelf: 'center'}}>This is what we do</Text>
+        <Image source={require('./assets/people_working.png')} style={{width: 600, height: 400, alignSelf: 'center'}} />
+      </View>
+      <View style={[styles.light, {height: 669, paddingRight: 40}]}>
+        <Text id="Offer Header" style={{alignSelf: 'center', fontSize: 35, padding: 40, paddingBottom: 20}}>What We Offer</Text>
+        <View id="Padding" style={{paddingBottom: 50}}>
+          <View id="Underline" style={{backgroundColor: '#35BBA8', alignSelf: 'center', width: 196, height: 5}}></View>
+        </View>
+        <Text id="Offer1" style={{color: "#496F7D", alignSelf: 'flex-start', fontSize: 18}}>
+          {`          Lorem ipsum dolor sit amet, 
+          consectetur adipiscing elit, sed do 
+          eiusmod tempor incididunt ut labore
+          et dolore magna aliqua. Ut enim 
+          ad minimveniam, quis nostrud 
+          exercitation ullamco laboris nisi
+          ut aliquip ex ea commodo`}
+        </Text>
+        <Text id="Offer1" style={{color: "#496F7D", alignSelf: 'flex-end', fontSize: 18}}>
+          {`          Lorem ipsum dolor sit amet, 
+          consectetur adipiscing elit, sed do 
+          eiusmod tempor incididunt ut labore
+          et dolore magna aliqua. Ut enim 
+          ad minimveniam, quis nostrud 
+          exercitation ullamco laboris nisi
+          ut aliquip ex ea commodo`}
+        </Text>
+        <Text id="Offer1" style={{color: "#496F7D", alignSelf: 'flex-start', fontSize: 18}}>
+          {`          Lorem ipsum dolor sit amet, 
+          consectetur adipiscing elit, sed do 
+          eiusmod tempor incididunt ut labore
+          et dolore magna aliqua. Ut enim 
+          ad minimveniam, quis nostrud 
+          exercitation ullamco laboris nisi
+          ut aliquip ex ea commodo`}
+        </Text>
+      </View>
+      <View style={styles.contacts}>
+        <Text style={{color: 'white', fontSize: 30, padding: 10, paddingBottom: 30}}>Contact Us</Text>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Image source={require('./assets/email.png')} style={{width: 33, height: 33}} />
+          <Text style={{color: 'white', fontSize: 20, paddingRight: 100}}>umd.dreamx@gmail.com</Text>
+          
+          <Image source={require('./assets/instagram.png')} style={{width: 30, height: 30}} />
+          <Text style={{color: 'white', fontSize: 20}}>@umd.dreamx</Text>
         </View>
       </View>
-      );
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -20,9 +65,16 @@ const styles = StyleSheet.create({
   },
 
   light: {
-    backgroundColor: '#FFFFFF',
+    backgrounColor: 'white',
+    color: "#496F7D",
+  },
 
-  }
+  contacts: {
+    backgroundColor: "#496F7D",
+    height: 150,
+    color: 'white',
+    alignItems: 'center',
+  },
 });
 
 export default MyApp;
--- RNPractice/App.js
+++ RNPractice/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View} from 'react-native';
+import { Button, StyleSheet, Text, View, Image} from 'react-native';
 import { useState } from 'react';
 
 
@@ -7,27 +7,27 @@ const MyApp = () => {
   const [hours, setHours] = useState(0);
   const [workDone, setWorkDone] = useState(false);
   return (
-      <View id="Parent View">
-        <View style={styles.dark}>
-          <p id="par">Suhas made this website. {myFunction("Suhas", 19)}. <br></br>
-                      Suhas has worked for {hours} hours today. <br></br>
-          </p>
-        </View>
-        <Button title={"Click to increase hours worked"}
-                onPress={() => {setHours(hours + 1)}}
-                disabled={workDone}></Button>
-        <Button title={"Press to decrease hours worked"}
-                onPress={() => {setHours(hours - 1)}}
-                disabled={workDone}></Button>
-        <Button id={"ligma"} title={workDone? "Work Completed": hours >= 4? "Click to complete work" : "Work not yet complete"}
-                color={hours >= 4? 'green': 'red'}
-                disabled={workDone}
-                onPress={() => {if (hours >= 4) {
-                                  setWorkDone(true);
-                                }}}>
-        </Button>
+    <View id="Parent View">
+      <View style={styles.dark}>
+        <p id="par">Suhas made this website. {myFunction("Suhas", 19)}. <br></br>
+                    Suhas has worked for {hours} hours today. <br></br>
+        </p>
       </View>
-      );
+      <Button title={"Click to increase hours worked"}
+              onPress={() => {setHours(hours + 1)}}
+              disabled={workDone}></Button>
+      <Button title={"Press to decrease hours worked"}
+              onPress={() => {setHours(hours - 1)}}
+               disabled={workDone}></Button>
+       <Button id={"ligma"} title={workDone? "Work Completed": hours >= 4? "Click to complete work" : "Work not yet complete"}
+              color={hours >= 4? 'green': 'red'}
+              disabled={workDone}
+              onPress={() => {if (hours >= 4) {
+                                setWorkDone(true);
+                              }}}>
+      </Button>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
